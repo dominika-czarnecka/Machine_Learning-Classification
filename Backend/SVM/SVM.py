7e695d4170fc6d5503070f7025c94d2a1dbe9ed9
tomfen <tomfen999@gmail.com>
2017-04-10 23:16:15 +0200

    Prototyp SVM
    prototyp klasy realizującej SVM

--- Backend/SVM/SVM.py
+++ Backend/SVM/SVM.py
@@ -1,22 +1,42 @@
 from sklearn import svm
 
 class ClassificatorInterface(object):
-    dict = {}
-    dict_rev = []
 
     def __init__(self, corpus):
-        self.clf = svm.SVC(kernel='linear')
         self.corpus = corpus
+        self._dict = {}
+        self._dict_rev = []
+        self.kernel_type = 'linear'
 
     def train(self, args, count):
+        self.create_dictionary()
+
+        xs = []
+        ys = []
+        for doc in corpus.docs():
+            xs.append(self.doc2vec(doc))
+            ys.append(doc.category)
+
+        self.clf = svm.SVC(self.kernel)
+        self.clf.fit(xs, ys)
+
+    def create_dictionary(self):
         i = 0
         for word in corpus.get_unique_words():
-            
-
+            self._dict[word] = i
+            self._dict_rev[i] = word
+            i += 1
 
     def classify(self, document):
         vec = self.doc2vec(document)
         return self.clf.predict(vec)
 
     def doc2vec(self, document):
-        raise Exception("NotImplementedException")
+        vect = []
+
+        for word in document:
+            if self._dict.has_key(word):
+                i = self[word]
+                vect[i] = 1
+
+        return vect
