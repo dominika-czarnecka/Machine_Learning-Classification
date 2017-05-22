2e99c17a785de3dbf3525cae64845b3a72bb9b17
wbelka94 <wbelka94@gmail.com>
2017-05-22 21:32:54 +0200

    update documentClassificationView, testClassifierView

--- Frontend/documentClassificationView.py
+++ Frontend/documentClassificationView.py
@@ -11,24 +11,18 @@ class documentClassificationView:
 
     def initComponents(self):
         self.mainFrame = Frame(self.tk, padx=5, pady=5)
-        self.mainFrame.pack(fill=Y)
+        self.mainFrame.pack(fill=X)
 
-        B = Button(self.mainFrame, text='Back', command=self.onClickBack)
-        B.pack(anchor=NW)
+        self.backButton = Button(self.mainFrame, text='Back', command=self.onClickBack)
+        self.backButton.pack(anchor=NW, side=LEFT)
 
-        # classifierChooser
-        self.labelFrame_1 = LabelFrame(self.mainFrame, text="Select a classifier:", width=10)
+        self.selectedClassifier = StringVar(self.tk)
+        self.selectedClassifier.set("Choose Classifier")
 
-        self.selectedClassifier = IntVar()
-        self.selectedClassifier.set(1)
+        self.dropDown = OptionMenu(self.mainFrame, self.selectedClassifier, "SVM", "Word2Vec", "Neuron")
+        self.dropDown.config(width=15)
+        self.dropDown.pack(anchor=SE)
 
-        self.labelFrame_1.pack(anchor=W, fill=X)
-
-        # classifierOptions
-        self.classifierOptionsFrame = LabelFrame(self.mainFrame, text="Classifier options:")
-        self.classifierOptionsFrame.pack(anchor=W, fill=X)
-
-        # document for classification
         self.labelFrame_3 = LabelFrame(self.mainFrame, text="Document:", padx=5, pady=5)
         self.textField_1 = Text(self.labelFrame_3, width=70, height=30)
         self.textField_1.pack(anchor=W, fill=X)
@@ -42,7 +36,6 @@ class documentClassificationView:
         self.mv.show()
 
     def onClickClassify(self):
-        # p = progress.progressWindow()
         return
 
     def hide(self):
--- Frontend/testClassifierView.py
+++ Frontend/testClassifierView.py
@@ -10,16 +10,35 @@ class testClassifierView:
         self.initComponents()
 
     def initComponents(self):
-        self.mainFrame = Frame(self.tk, padx=5, pady=5)
+        self.mainFrame = Frame(self.tk, padx=2,pady=5)
         self.mainFrame.pack(fill=X)
+
 
         B = Button(self.mainFrame, text='Back', command=self.onClickBack)
         B.pack(anchor=NW)
 
+        self.lebelClassifier = Label(self.mainFrame, text="Choose Classifier Options:")
+        self.lebelClassifier.pack(anchor=CENTER, pady=10)
 
+        self.selectedClassifier = StringVar()
+        self.selectedClassifier.set("Choose option")
 
-        self.testButton = Button(self.mainFrame, text='Test', command=self.onClickTest)
-        self.testButton.pack(anchor=SE)
+        self.dropDown = OptionMenu(self.mainFrame, self.selectedClassifier, "SVM", "Word2Vec", "Neuron")
+        self.dropDown.config(width=25,padx=50)
+        self.dropDown.pack(pady=10)
+
+        self.lebelTestDocuments = Label(self.mainFrame, text="Documents for testing:")
+        self.lebelTestDocuments.pack(anchor=CENTER)
+
+        self.selectedDocumentForTest = StringVar()
+        self.selectedDocumentForTest.set("Choose option")
+
+        self.dropDown = OptionMenu(self.mainFrame, self.selectedDocumentForTest, "Reuters")
+        self.dropDown.config(width=25,padx=50)
+        self.dropDown.pack(pady=20)
+
+        self.testButton = Button(self.mainFrame, width=70, height= 5, text='Test', command=self.onClickTest)
+        self.testButton.pack(anchor=N, pady=30)
 
     def onClickBack(self):
         self.hide()
